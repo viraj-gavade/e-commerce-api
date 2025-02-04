fab81f86e619541f5470857d34c4cb42065405a7
Viraj Gavade <vrajgavade17@gmail.com>
2025-02-04 13:06:06 +0530

    Some deployment configurations

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,25 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import 'dotenv/config';
-
+import * as serverless from 'serverless-http';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  console.log(process.env.JWT_SECRET)
-  app.use(cookieParser())
-  await app.listen(process.env.PORT ?? 3000);
+  console.log(process.env.JWT_SECRET); // Log your JWT_SECRET to check if it's set properly
 
+  // Use cookie parser middleware
+  app.use(cookieParser());
+
+  // Initialize the app but do not listen on a port
+  await app.init();
+
+  // Use serverless-http to adapt the app for serverless
+  const handler = serverless(app.getHttpAdapter().getInstance());
+
+  // Export the handler for serverless function
+  module.exports.handler = handler;
 }
-bootstrap();
+
+bootstrap().then(() => {
+  console.log("Serverless handler initialized");
+});
